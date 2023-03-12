521a9d3e6c7d34fa0e86f988efbc7630dbafd83e
yuxin-dell-xps <yuxin_liu_1998@outlook.com>
2023-03-12 16:32:58 +0800

    set up and add ValuesVariableTypes

---
+++ src/lectures/part1basic/ValuesVariableTypes.scala
@@ -0,0 +1,17 @@
+package lectures.part1basic
+
+object ValuesVariableTypes extends App{
+  val x: Int = 42
+  println(x)
+  // vals are immutable
+  // types are optional
+  val aString:String ="hello";
+  val aBoolean:Boolean = false;
+  val aShort:Short = 42
+  val aLong:Long = 72837283L
+  val aFloat:Float = 2.0f
+  val aDouble:Double =3.14
+
+
+
+}
---
+++ src/playground/palyground.scala
@@ -0,0 +1,9 @@
+package playground
+
+object palyground {
+  def main(args: Array[String]): Unit = {
+    // main enter
+    println("hello world")
+  }
+
+}
